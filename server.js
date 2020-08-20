af7ee172d810551ff3641a0d92d2094813cd7846
Minakshi Kumar <minakshiakumar23@gmail.com>
2020-08-20 16:46:05 +0530

    server creation & tag UI extension code

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+app.use(express.static(path.join(__dirname, 'public')));
+console.log("__dirname", __dirname);
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/control.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
